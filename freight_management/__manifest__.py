1555c394453777a1f37918e1d7f1e712fd3e8f0f
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-09-16 13:53:22 +0530

    Bugs

--- freight_management/__manifest__.py
+++ freight_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Freight Request Management',
-    'version': '14.0.0.8',
+    'version': '14.0.0.9',
     'category': 'Freight',
     'description': """
                 Freight Management
