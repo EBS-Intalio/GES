14a08e036ddb4370df7b34d2467530ebe6cbce70
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-09-22 14:04:40 +0530

    [ADded] a is type field

--- freight_management/__manifest__.py
+++ freight_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Freight Request Management',
-    'version': '14.0.1.2',
+    'version': '14.0.1.3',
     'category': 'Freight',
     'description': """
                 Freight Management
