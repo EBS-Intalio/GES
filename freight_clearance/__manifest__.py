8cfe2aa2e7c8d136d2863a88e0306f838109e35b
Hasanali Aghariya <72130262+HasanaliTechUltra@users.noreply.github.com>
2021-09-22 13:52:41 +0530

    Update manifest file.

--- freight_clearance/__manifest__.py
+++ freight_clearance/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Freight Clearance',
-    'version': '14.0.0.3',
+    'version': '14.0.0.4',
     'category': 'Freight',
     'description': """
                 Freight Clearance
