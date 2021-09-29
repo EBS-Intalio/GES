37cc36b0984943e7c3aff74f118149b77d412973
dipen <dipen.techultra@gmail.com>
2021-09-29 21:00:49 +0530

    [ADD] required from xml

--- freight_management/__manifest__.py
+++ freight_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Freight Request Management',
-    'version': '14.0.1.12',
+    'version': '14.0.1.13',
     'category': 'Freight',
     'description': """
                 Freight Management
