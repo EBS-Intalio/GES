194a0cb1e9cf46496d24546844dda0bff9fb462e
Mahmoud Ibrahim <m.ahmed@ever-bs.com>
2021-09-29 10:04:24 +0200

    [REF]freight_pricing_extention:Refactor Moudle

--- freight_pricing_extention/models/freight_pricing.py
+++ freight_pricing_extention/models/freight_pricing.py
@@ -8,8 +8,21 @@ class FreightPricingInherit(models.Model):
     _description = 'Freight Pricing'
     _order = 'name desc, id desc'
 
-    carrier_count = fields.Integer(string='Total carrier', compute='count_carriers', default=0)
+    first_carrier_id = fields.Many2one('res.partner', string="Carrier 1")
+    second_carrier_id = fields.Many2one('res.partner', string="Carrier 2")
+    third_carrier_id = fields.Many2one('res.partner', string="Carrier 3")
+    first_flight_no = fields.Char("Flight No")
+    second_flight_no = fields.Char("Flight No")
+    third_flight_no = fields.Char("Flight No")
+    first_vessel_id = fields.Many2one('freight.vessel', 'Vessel')
+    second_vessel_id = fields.Many2one('freight.vessel', 'Vessel')
+    third_vessel_id = fields.Many2one('freight.vessel', 'Vessel')
+    first_rout = fields.Char('Rout')
+    second_rout = fields.Char('Rout')
+    third_rout = fields.Char('Rout')
 
-    @api.depends('carrier_ids')
-    def count_carriers(self):
-        self.carrier_count = len(self.carrier_ids)
+    # carrier_count = fields.Integer(string='Total carrier', compute='count_carriers', default=0)
+
+    # @api.depends('carrier_ids')
+    # def count_carriers(self):
+    #     self.carrier_count = len(self.carrier_ids)
