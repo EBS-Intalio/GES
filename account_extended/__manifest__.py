2c8d14835f846a64342df2daa80cec26859dc3b6
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-11-11 16:56:51 +0530

    [IMP]ADded dependency of web

--- account_extended/__manifest__.py
+++ account_extended/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Account Extended',
-    'version': '14.0.0.8',
+    'version': '14.0.0.9',
     'category': 'Accounting/Accounting',
     'description': """
                 Multi currency according to the company timezone
                """,
     'sequence': 51,
-    'depends': ['account', 'base','account_reports'],
+    'depends': ['account', 'base','account_reports','web'],
     'author': 'Ever Business Solutions',
     'website': "http://www.ever-bs.com/",
     'data': [
