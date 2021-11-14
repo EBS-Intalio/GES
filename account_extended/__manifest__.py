a959cb0342506b10ea73a4c1b793ade25a403e32
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-11-14 15:20:59 +0530

    [IMP]Changes in calculation of bank statement

--- account_extended/__manifest__.py
+++ account_extended/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Account Extended',
-    'version': '14.0.0.12',
+    'version': '14.0.0.13',
     'category': 'Accounting/Accounting',
     'description': """
                 Multi currency according to the company timezone
