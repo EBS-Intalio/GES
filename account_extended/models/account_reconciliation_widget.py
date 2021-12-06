ba6d7c25351aa45349133b819f10c974312a1cf6
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-12-06 15:55:44 +0530

    [IMP]SOlved a bug

--- account_extended/models/account_reconciliation_widget.py
+++ account_extended/models/account_reconciliation_widget.py
@@ -4,8 +4,8 @@ class AccountReconciliationExtended(models.AbstractModel):
     _inherit = 'account.reconciliation.widget'
 
     @api.model
-    def _prepare_js_reconciliation_widget_move_line(self, statement_line, line):
-        js_vals = super(AccountReconciliationExtended, self)._prepare_js_reconciliation_widget_move_line(statement_line=statement_line,line=line)
+    def _prepare_js_reconciliation_widget_move_line(self, statement_line, line,recs_count=0):
+        js_vals = super(AccountReconciliationExtended, self)._prepare_js_reconciliation_widget_move_line(statement_line=statement_line,line=line,recs_count=recs_count)
         js_vals.update({
             'payment_ref': line.payment_ref or '',
             'check_ref': line.check_ref or '',
