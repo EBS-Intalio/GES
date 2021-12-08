67a960c4edd3f6cc435d363550d31641fd8d21aa
HasanaliTechUltra <hasanali.tus@gmail.com>
2021-12-08 18:22:35 +0530

    [CHANGE]Added optional hide in move lines

--- account_extended/__manifest__.py
+++ account_extended/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Account Extended',
-    'version': '14.0.0.19',
+    'version': '14.0.0.20',
     'category': 'Accounting/Accounting',
     'description': """
                 Multi currency according to the company timezone
